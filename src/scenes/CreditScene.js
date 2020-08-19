c2893a2ea264db93e75737d25699da7695d6a53f
frankopkusianwar <okirorfrank3@gmail.com>
2020-08-19 11:26:24 +0300

    Fix credits scene bug

--- src/scenes/CreditScene.js
+++ src/scenes/CreditScene.js
@@ -1,5 +1,5 @@
 import 'phaser';
-import config from '../Config/config';
+import config from '../config/config';
 
 export default class CreditsScene extends Phaser.Scene {
   constructor() {
@@ -9,7 +9,10 @@ export default class CreditsScene extends Phaser.Scene {
   create() {
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
     this.madeByText = this.add.text(0, 0, 'Created By: Frank Okiror', { fontSize: '26px', fill: '#fff' });
+    this.madeByText1 = this.add.text(0, 0, 'Thanks to OpenGameArt.org for\n\nproviding the assets\n\nused in this game.', { fontSize: '26px', fill: '#fff' });
     this.zone = this.add.zone(config.width / 2, config.height / 2, config.width, config.height);
+    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.skipText = this.add.text(10, 10, 'Press SPACE\nto skip', { fontSize: '10px', fill: '#fff' });
 
     Phaser.Display.Align.In.Center(
       this.creditsText,
@@ -21,11 +24,17 @@ export default class CreditsScene extends Phaser.Scene {
       this.zone,
     );
 
-    this.madeByText.setY(1000);
+    Phaser.Display.Align.In.Center(
+      this.madeByText1,
+      this.zone,
+    );
 
+    this.madeByText.setY(1000);
+    this.madeByText1.setY(1200);
+    
     this.creditsTween = this.tweens.add({
       targets: this.creditsText,
-      y: -100,
+      y: -200,
       ease: 'Power1',
       duration: 3000,
       delay: 1000,
@@ -36,14 +45,30 @@ export default class CreditsScene extends Phaser.Scene {
 
     this.madeByTween = this.tweens.add({
       targets: this.madeByText,
-      y: -300,
+      y: -200,
       ease: 'Power1',
       duration: 8000,
       delay: 1000,
       onComplete: function () {
-        this.madeByTween.destroy;
+        this.destroy;
+      }
+    });
+
+    this.madeByTween1 = this.tweens.add({
+      targets: this.madeByText1,
+      y: -200,
+      duration: 8000,
+      delay: 1000,
+      onComplete: function () {
+        this.madeByTween1.destroy;
         this.scene.start('Title');
       }.bind(this),
     });
+    
+  }
+  update() {
+    if (this.keySpace.isDown) {
+      this.scene.start('Title');
+    }
   }
 }
