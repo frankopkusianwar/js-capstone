8c33bd43fbcff36d768405cefd699dece06827d6
frankopkusianwar <okirorfrank3@gmail.com>
2020-08-19 12:26:39 +0300

    Add tests for posting and getting api data

---
+++ src/__tests__/scoreBoard.test.js
@@ -0,0 +1,32 @@
+import { addScore, getScores } from '../objects/scoreBoard'
+
+it('should post an object to the games API', () => {
+  addScore().then(data => {
+    expect(typeof data).toBe('object');
+  });
+});
+
+it('should get username', () => {
+  getScores().then(data => {
+    expect(data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          user: 'frank',
+        }),
+      ]),
+    );
+  });
+});
+
+it('should get score', () => {
+  getScores().then(data => {
+    expect(data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          score: '300',
+        }),
+      ]),
+    );
+  }).catch(() => {});
+});
+
